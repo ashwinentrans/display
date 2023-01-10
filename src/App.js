34c8b0262c4e164908f9e2e3cbcd2499a258d3ca
ashwinentrans <ashwin@entrans.io>
2023-01-10 14:57:56 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
 function App() {
+  const [comments, setComments] = useState([])
+  const [search, setSearch] = useState("")
+  const getData = async() => {
+    try{
+      const data = await axios.get("https://jsonplaceholder.typicode.com/comments")
+
+      console.log("data",data.data)
+      setComments(data.data)
+    }catch(e){
+      console.log(e)
+    }
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Comments</h1>
+        <input type="text" placeholder="Search" onChange={(e)=>{
+          setSearch(e.target.value);
+        }}/>
+        <div style="overflow-x:auto;">
+          <table>
+            <tr>
+              <th>Post Id</th>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Body</th>
+            </tr>
+            {/* {comments
+            .filter((item)=>{
+              if (search==""){
+                return item;
+              }else if(item.name.toLowerCase().includes(search.toLowerCase())){
+                return item;
+              }
+            })
+            .map((item)=>{
+              return(
+                <tr key={item.id}>
+                  <td>{item.postId}</td>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.body}</td>
+                </tr>
+              );
+            })} */}
+            </table>
+        </div>
     </div>
   );
 }
